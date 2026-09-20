afd546aa798e4e8b7a44a12c394c03bd6ece70c2
BenDoe <bennacer.douirat.solicode@gmail.com>
2026-09-20 23:02:06 +0000

    container

--- anagram.sh
+++ anagram.sh
@@ -1,5 +1,70 @@
 #!/usr/bin/bash
 
+container=()
+filled=0
+
+contains(){
+local flag="$1"
+local word="$2"
+
+flag="${flag^^}"
+word="${word^^}"
+
+local anagram=1
+# local -A container
+
+# echo "flag: $flag"
+# echo "word: $word"
+
+if [[ "$flag" == "$word" ]]; then
+	echo 0
+	return
+fi
+
+if (( filled == 0 )); then
+
+for((i=0; i<${#flag}; i++)); do
+
+local x=${flag:i:1}
+
+container+=("$x")
+
+done
+
+filled=1
+
+fi
+
+
+
+for((i=0; i<${#flag}; i++)); do
+
+local x=${word:i:1}
+local v=$(exists "$x")
+
+if (( v != 1 )); then
+	anagram=0
+fi
+
+done
+
+echo "$anagram"
+}
+
+exists(){
+	local char="$1"
+	for((i=0; i<${#container[@]}; i++)); do
+		local x="${container[$i]}"
+		if [[ "$char" == "$x" ]]; then
+			container[i]=0
+			echo 1
+			return
+		fi
+	done
+	echo 0
+}
+
+
 main(){
 local x="$1"
 local y="$2"
@@ -50,47 +115,23 @@ if [[ "$sub" != ""  ]]; then
 fi
 
 for n in "${all[@]}"; do
+	echo "---> $n --> $filled"
+	echo "${container[@]}"
+
+	filled=0
 	local exist=$(contains "$x" "$n")
 	if ((exist == 1)); then
-		if [[ result != "" ]]; then
+		if ((${#result} > 0)); then
 			result+=" $n"
 			continue
-		fi
+		else
 		result+="$n"
+		fi
 	fi
 done
 echo "$result"
 }
 
-contains(){
-local flag="$1"
-local word="$2"
-
-local anagram=1
-local -A container
-
-# echo "flag: $flag"
-# echo "word: $word"
-
-for((i=0; i<${#flag}; i++)); do
-
-local x=${flag:i:1}
-
-container["$x"]=1
-
-done
 
 
-for((i=0; i<${#flag}; i++)); do
-
-local x=${word:i:1}
-local v=container[$x]
-
-if (( v != 1 )); then
-	anagram=0
-fi
-done
-
-echo "$anagram"
-}
 main "$@"
