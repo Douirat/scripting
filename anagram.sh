1a96c151d3162a306b2a130ed7a5b9e68ebc9644
BenDoe <bennacer.douirat.solicode@gmail.com>
2026-09-22 23:24:26 +0000

    anagram is done

--- anagram.sh
+++ anagram.sh
@@ -1,137 +1,99 @@
 #!/usr/bin/bash
 
-container=()
-filled=0
-
-contains(){
-local flag="$1"
-local word="$2"
-
-flag="${flag^^}"
-word="${word^^}"
-
-local anagram=1
-# local -A container
-
-# echo "flag: $flag"
-# echo "word: $word"
-
-if [[ "$flag" == "$word" ]]; then
-	echo 0
-	return
-fi
-
-if (( filled == 0 )); then
-
-for((i=0; i<${#flag}; i++)); do
-
-local x=${flag:i:1}
-
-container+=("$x")
-
-done
-
-filled=1
-
-fi
-
-
-
-for((i=0; i<${#flag}; i++)); do
-
-local x=${word:i:1}
-local v=$(exists "$x")
-
-if (( v != 1 )); then
-	anagram=0
-fi
-
-done
-
-echo "$anagram"
-}
-
-exists(){
-	local char="$1"
-	for((i=0; i<${#container[@]}; i++)); do
-		local x="${container[$i]}"
-		if [[ "$char" == "$x" ]]; then
-			container[i]=0
-			echo 1
-			return
-		fi
-	done
-	echo 0
-}
-
-
 main(){
-local x="$1"
-local y="$2"
+#anagram.sh "solemn" "lemons cherry melons"
+local flag="$1"
+local args="$2"
 
-local all=()
-local result=""
+# store all the letters of the flag in an array to use them as a map.
+local map=()
 
-# anagram.sh "solemn" "lemons cherry melons"
-
-
-# echo "x: $x --> y: $y"
-
-local chars=()
-
-for((i=0; i<${#x}; i++)); do
-
-chars["$i"]="${x:i:1}"
-
+for((i=0; i<${#flag}; i++)); do
+	local x=${flag:i:1}
+	map+=("$x")
 done
 
-# echo "${chars[@]}"
+# echo "${map[@]}"
 
+# extract all the words from the string and store them in the array
+local words=()
 local sub=""
 
-for((j=0; j<${#y}; j++)); do
-
-local c=${y:j:1}
-
-if [[ "$c" == " " ]]; then
-	if [[ "$sub" != "$x" && ${#sub} == ${#x} ]]; then
-		all+=("$sub")
+for((i=0; i<${#args}; i++)); do
+	local char=${args:i:1}
+	if [[ "$char" == " " ]]; then
+		if [[ "${sub^^}" != "${flag^^}" ]]; then
+			if (( ${#sub} == ${#flag} )); then
+				words+=("$sub")
+			fi
+		fi
+		sub=""
+		continue
 	fi
-	sub=""
-	continue
-fi
-
-
-
-sub+="$c"
+	sub+="$char"
 done
 
-
-if [[ "$sub" != ""  ]]; then
-	if [[ "$sub" != "$x" && ${#sub} == ${#x}  ]]; then
-		all+=("$sub")
-	fi
-	sub=""
+if [[ "$sub" != "" &&  "${sub^^}" != "${flag^^}" ]]; then
+		if (( ${#sub} == ${#flag} )); then
+			words+=("$sub")
+		fi
 fi
 
-for n in "${all[@]}"; do
-	echo "---> $n --> $filled"
-	echo "${container[@]}"
+# echo "${words[@]}"
 
-	filled=0
-	local exist=$(contains "$x" "$n")
-	if ((exist == 1)); then
-		if ((${#result} > 0)); then
-			result+=" $n"
-			continue
+# colone the map to treat each word specifically:
+local clone=("${map[@]}")
+
+# echo "${clone[@]}"
+
+local result=""
+
+for word in ${words[@]}; do
+# echo "---> $word"
+
+	local accepted=1
+
+	for((i=0; i<${#word}; i++)); do
+		local c=${word:i:1}
+		local shouldBe=0
+
+			# echo "$c"
+		for (( x=0; x<${#clone[@]}; x++)); do
+			local z="${clone[$x]}"
+			#  echo "$x"
+			if [[ "${c^^}" == "${z^^}" ]]; then
+				shouldBe=1
+				clone["$x"]=""
+				break
+			fi
+		done
+
+		if (( shouldBe == 0 )); then
+			accepted=0
+			break
+		fi
+
+
+
+
+	done
+
+	if (( accepted == 1)); then
+		if((${#result} == 0 )); then
+			result+="$word"
 		else
-		result+="$n"
+		result+=" $word"
 		fi
 	fi
+
+
+	clone=("${map[@]}")
+	# echo "${clone[@]}"
+
 done
+
 echo "$result"
+
 }
-
-
 
 main "$@"
